852907e9607473dc4a45c1ef6d38fa0b40339d9f
nikita3006 <nikitanegi3011@gmail.com>
2024-01-13 00:07:03 +0530

    change commit

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
+server.use(middlewares)
+server.use(jsonServer.rewriter({
+    '/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 
 server.use(router)
 server.listen(3000, () => {
